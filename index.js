3bba44d5088af7ff0e57538827a827a88144c158
Maryanne Wambugu <2wambugu.marie@gmail.com>
2023-03-29 20:22:05 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(_feet){
   // Write your code here!
+  if (_feet <= 400) {
+    return 'This one is on me!';
+  }
+  if (_feet > 2000 &&  _feet < 2500) {
+    return 'I will gladly take your thirty bucks.';
+  }
+  else if (_feet > 2500) {
+    return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(_city){
   // Write your code here!
+  if (_city === 'NYC') {
+    return 'Ok, sounds good.'
+  }
+  else if (_city !== 'NYC') {
+    return 'No go.'
+  }
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(_tip){
   // Write your code here!
+  if (_tip === 'generous') {
+    return 'Thank you so much.'
+  } else if(_tip === 'not as generous') {
+    return 'Thank you.'
+  } else return 'Bye.'
 }
